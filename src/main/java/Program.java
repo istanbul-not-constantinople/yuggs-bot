dfd1bfa923ba446c47ae2de25de5afd5d1f2d0af
steve <thesethskigamer@gmail.com>
2021-04-30 18:48:23 +0100

    oops token

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -5,7 +5,7 @@ import javax.security.auth.login.LoginException;
 
 public class Program {
   public static void main(String[] args) throws LoginException {
-    JDA jda = JDABuilder.createDefault("NzU3MzIyMDQ3NjAzODY3Nzc4.X2etHg.ka3Su-4gIX_mzWlTfu63xiTUago").build();
+    JDA jda = JDABuilder.createDefault(args[0]).build();
 
     jda.addEventListener();
   }
